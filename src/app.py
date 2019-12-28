4d185bbfd1ab70b08f4104629f49cc5eed125aa0
senkaPro <isenatpro@gmail.com>
2019-12-28 03:42:27 +0000

    Added base template

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///shortener.db")
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('base.html')
