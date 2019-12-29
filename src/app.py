a08bf9ba86dfc748a33ab92bad9118ebe1508578
senkaPro <isenatpro@gmail.com>
2019-12-29 02:07:43 +0000

    Added login and register form templates

--- src/app.py
+++ src/app.py
@@ -11,4 +11,12 @@ db = SQL("sqlite:///shortener.db")
 
 @app.route("/")
 def hello():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
